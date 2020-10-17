79938af103e4acb1e8c7a09695238bd051800583
Aishwarya Bhosale <aishwaryabhosle438@gmail.com>
2020-10-17 16:54:05 -0700

    code beautifying changes

--- CECS450_Project1.py
+++ CECS450_Project1.py
@@ -1,31 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-'''from wordcloud import WordCloud, STOPWORDS 
-import matplotlib.pyplot as plt 
-import pandas as pd 
-
-comment_words=input("Type something to test this out: ")
-  
-wordcloud = WordCloud(width = 800, height = 800, 
-                background_color ='white', 
-                stopwords = STOPWORDS, 
-                min_font_size = 10).generate(comment_words) 
-
-# plot the WordCloud image                        
-plt.figure(figsize = (8, 8), facecolor = None) 
-plt.imshow(wordcloud) 
-plt.axis("off") 
-plt.tight_layout(pad = 0)
-plt.show() 
-
-'''
-# In[53]:
-
-
 from collections import Counter
 import re
 from flask import Flask, jsonify, request, render_template
@@ -93,10 +65,7 @@ def hello():
 
     # GET request
     else:
-        #print(comment_words)   
-        #print("Dict")
         finalDict=generateWord(comment_words)
-
         #print(finalDict)
         return jsonify(dict=finalDict)  # serialize and use JSON headers
 
@@ -107,48 +76,4 @@ def test_page():
 
 if __name__ == "__main__":
 	app.run()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-'''
-from flask import Flask, jsonify, request, render_template
-#from flask_cors import CORS
-
-app = Flask(__name__)
-#CORS(app)
-
-@app.route('/hello', methods=['GET', 'POST'])
-def hello():
-
-    # POST request
-    if request.method == 'POST':
-        print('Incoming..')
-        print(request.get_json())  # parse as JSON
-        return 'OK', 200
-
-    # GET request
-    else:
-        message = {'greeting':'Hello from Flask!'}
-        print('****'+jsonify(message))
-        return jsonify(message)  # serialize and use JSON headers
-
-@app.route('/test')
-def test_page():
-    # look inside `templates` and serve `index.html`
-    return render_template('frontEnd.html')
-if __name__ == "__main__":
-	app.run()
-'''
-
-# In[ ]:
-
-
-
 
